6be4d0670cae180c1e9d6b8575d3f3c2034dc337
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-23 13:16:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,8 @@
 class Person
-  attr_accessor :name, :hair_color, :eye_color, :weight, :birthday
+  attr_accessor :name, :hair_color, :eye_color, :weight, :birthday, :height, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size :pant_length, :pant_width
   
   def initialize (attributes)
    attributes.each {|key, value| self.send(("#{key}="), value)}
 end
+
+person.new
