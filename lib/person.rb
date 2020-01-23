252dbfc39b661486a3e1bc196d06793606127bb4
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-23 13:18:13 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  #your code here
+  attr_accessor :name, :hair_color, :eye_color, :weight, :birthday, :height, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size :pant_length, :pant_width
+  
+  def initialize (attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
 end
+
+bob = person.new(bob_attributes)
